128af47646140303fe774c6b3bb3d6b401442480
yamori-masato <yamorimasato@gmail.com>
2021-05-16 18:59:12 +0900

    add event listeners

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 import p5 from 'p5'
 import sketch from './sketch'
+import FastClick from 'fastclick'
 
 new p5(sketch)
+
+// スクロール禁止
+document.addEventListener("touchmove", e => e.preventDefault(), { passive: false })
+document.addEventListener("mousewheel", e => e.preventDefault(), { passive: false })
+
+// タップ判定高速化
+document.addEventListener('DOMContentLoaded', function() {
+  (FastClick as any).attach(document.body)
+}, false)
